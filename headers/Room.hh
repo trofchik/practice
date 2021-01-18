8b57ea701d70288ab90b06d49583a0b2c9b57a28
trofchik <trofimov.nikita20@gmail.com>
2021-01-18 10:07:26 +0100

    Update Room.hh

--- headers/Room.hh
+++ headers/Room.hh
@@ -3,18 +3,10 @@
 #include <variant>
 #include <iostream>
 
-using ContentType = std::variant<Party>;
-using HVector = std::vector<ContentType>;
-
 class Room
 {
 public:
-    
     Room();
     ~Room();
-    void sortContByPriority();
-    
-private:
-    HVector content;
 };
 
